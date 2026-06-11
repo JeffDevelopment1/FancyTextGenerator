cc6d8f14d341714678f98c8347fb507fd3fc303a
AlexDevelopment <kaigt1237@gmail.com>
2026-06-11 17:31:17 +0100

    Update FontGenerator.cs

--- Services/FontGenerator.cs
+++ Services/FontGenerator.cs
@@ -1,1 +1,41 @@
+using System.Text;
 
+namespace FancyTextGenerator.Services;
+
+public static class FontGenerator
+{
+    private const string Normal = "abcdefghijklmnopqrstuvwxyz";
+
+    public static readonly Dictionary<string, string> Fonts = new()
+    {
+        ["Bold"] = "𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳",
+        ["Italic"] = "𝑎𝑏𝑐𝑑𝑒𝑓𝑔ℎ𝑖𝑗𝑘𝑙𝑚𝑛𝑜𝑝𝑞𝑟𝑠𝑡𝑢𝑣𝑤𝑥𝑦𝑧",
+        ["Bold Italic"] = "𝒂𝒃𝒄𝒅𝒆𝒇𝒈𝒉𝒊𝒋𝒌𝒍𝒎𝒏𝒐𝒑𝒒𝒓𝒔𝒕𝒖𝒗𝒘𝒙𝒚𝒛",
+        ["Script"] = "𝒶𝒷𝒸𝒹ℯ𝒻ℊ𝒽𝒾𝒿𝓀𝓁𝓂𝓃ℴ𝓅𝓆𝓇𝓈𝓉𝓊𝓋𝓌𝓍𝓎𝓏",
+        ["Fancy Script"] = "𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃",
+        ["Fraktur"] = "𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷",
+        ["Double Struck"] = "𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫",
+        ["Monospace"] = "𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣",
+        ["Bubble"] = "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ",
+        ["Small Caps"] = "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ",
+        ["Full Width"] = "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ",
+        ["Squared"] = "🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅈🅉",
+        ["Circled"] = "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ",
+        ["Tiny"] = "ᵃᵇᶜᵈᵉᶠᵍʰᶦʲᵏˡᵐⁿᵒᵖᵠʳˢᵗᵘᵛʷˣʸᶻ",
+        ["Gothic Bold"] = "𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟",
+        ["Wide"] = "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"
+    };
+
+    public static string Convert(string input, string map)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var c in input)
+        {
+            int i = Normal.IndexOf(char.ToLower(c));
+            sb.Append(i >= 0 ? map[i] : c);
+        }
+
+        return sb.ToString();
+    }
+}
